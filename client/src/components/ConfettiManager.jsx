bbaa6553a8c1cc53873e42c4144944778e9d8a4d
Prit Zalavadiya <pritzalavadiya0@gmail.com>
2026-01-16 20:34:19 +0530

    Refactor ConfettiManager component and clean up code

--- client/src/components/ConfettiManager.jsx
+++ client/src/components/ConfettiManager.jsx
@@ -1,8 +1,6 @@
 import React, { useEffect, useState } from 'react';
 import Confetti from 'react-confetti';
-import { useWindowSize } from 'react-use'; // npm install react-use
-
-// Note: You need to install: npm install react-use react-confetti
+import { useWindowSize } from 'react-use';
 
 const ConfettiManager = ({ trigger, setTrigger }) => {
   const { width, height } = useWindowSize();
@@ -11,10 +9,9 @@ const ConfettiManager = ({ trigger, setTrigger }) => {
   useEffect(() => {
     if (trigger) {
       setShow(true);
-      // Stop the confetti rain after 5 seconds
       const timer = setTimeout(() => {
         setShow(false);
-        setTrigger(false); // Reset parent state
+        setTrigger(false);
       }, 5000);
       return () => clearTimeout(timer);
     }
